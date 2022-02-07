7fbe4d37f90a1a3bc3bc283cfe1c5fb0d67f5f86
nickblank <98602112+nickblank@users.noreply.github.com>
2022-02-07 10:28:01 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	    String partyAfil = "l";
+
+        if(partyAfil == "D")
+        {
+            System.out.println("You’re a Democratic Donkey.");
+        }
+        else if(partyAfil == "E")
+        {
+            System.out.println("You’re a Republican Elephant.");
+        }
+        else if(partyAfil == "I")
+        {
+            System.out.println("You’re an Independent Man.");
+        }
+        else
+        {
+            System.out.println("You chose other!");
+        }
+    }
+}
